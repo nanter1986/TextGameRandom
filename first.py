77515eb0b60da76ab83cdec5157411d2af2ea0e6
nanter1986 <nanter1986@gmail.com>
2019-11-05 14:06:43 +0200

    response to input direction

--- first.py
+++ first.py
@@ -1,3 +1,9 @@
 print("You are here")
 print("What do you want to do")
 direction=input("l for left,r for right")
+if direction=="l":
+    print("You went left")
+elif direction=="r":
+    print("You went right")
+else:
+    print("uknown command")
