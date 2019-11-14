5d4017d65743f0e342179073b9c4064c4aee27f3
nanter1986 <nanter1986@gmail.com>
2019-11-14 06:57:13 +0200

    room generator creates random number of furniture.should be added

--- roomGenerator.py
+++ roomGenerator.py
@@ -1,6 +1,11 @@
-class Room:
-    def __init__(self):
-        pass
+import random
+import npcObjects
 
+class Room:
     furniture=[]
-    npcs=[]
+    enemies=[]
+    def __init__(self):
+        for i in range(0,10):
+            number=random.randint(0,3)
+            if number>0:
+                furniture.append(npcObjects())
