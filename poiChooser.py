2ed926208e23c13ecb4222f44ca0d5a9128c6df2
nanter1986 <nanter1986@gmail.com>
2019-11-28 07:18:25 +0200

    added some chair options

--- poiChooser.py
+++ poiChooser.py
@@ -5,7 +5,7 @@ def getTablePois():
     randonNumber=randbelow(len(options))
 
 def getChairPois():
-    options=[]
+    options=["pillow","wood"]
     randonNumber=randbelow(len(options))
 
 def choosePointsOfInterest(name):
